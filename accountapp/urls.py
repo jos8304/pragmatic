6e8c4c6601845b478a7b4186f6f29f5583ced618
jos8304 <jyou8304@gmail.com>
2024-11-12 19:00:39 -0500

    add env file

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,13 @@
 
 from django.urls import path
 from .views import AccountCreateView
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = "accountapp"
 
 urlpatterns = [
-    path('create/', AccountCreateView.as_view(), name='account_create'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 
 ]
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -25,10 +25,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1b3%3&r#_=m6#)n8n+yo&ck1&&394!!2aoshv7viig+4%86wlr'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
